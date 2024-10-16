6a74395d3756b46d1b01c2075f338e0bf837724d
pmaxaa <penckowamaria198@gmail.com>
2024-10-17 00:11:26 +0700

    init

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import { AuthPage } from '../pages/auth'
+
+function App() {
+  return (
+    <>
+      <AuthPage/>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/auth/ui/AuthPage.tsx
@@ -0,0 +1,5 @@
+import { Button } from '@/shared/ui/Button';
+
+export function AuthPage() {
+  return <Button>кнопка</Button>;
+}
